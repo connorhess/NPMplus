21e6c3904ca240e6d30adbb3e33873b6438b70b8
Connor <hessconnor41@gmail.com>
2025-11-09 16:16:24 +0200

    set default value for INITIAL_DEFAULT_PAGE in setupDefaultSettings function

--- backend/setup.js
+++ backend/setup.js
@@ -94,7 +94,7 @@ const setupDefaultSettings = () => {
 							id: "default-site",
 							name: "Default Site",
 							description: "What to show when Nginx is hit with an unknown Host",
-							value: process.env.INITIAL_DEFAULT_PAGE,
+							value: process.env.INITIAL_DEFAULT_PAGE || "congratulations",
 							meta: {},
 						})
 						.then(() => {
